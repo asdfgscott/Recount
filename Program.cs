08ae3ef81564d447682e5f342bfb9f61b282da1f
Scott Truong <asdfgscott@gmail.com>
2021-10-03 19:22:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
